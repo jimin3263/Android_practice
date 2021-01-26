c0d26ed5788249a561ed1625b745f83e3e37a53a
jimin3263 <dkdldsla0000@naver.com>
2021-01-27 04:09:16 +0900

    practice

---
+++ layout_practice/app/src/main/java/org/techtown/layout_practice/MainActivity.java
@@ -0,0 +1,73 @@
+package org.techtown.layout_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                String message = editText.getText().toString();
+                Toast.makeText(getApplicationContext(),"전송할 메시지\n\n"+message,Toast.LENGTH_LONG).show();
+            }
+        });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+
+        TextWatcher watcher = new TextWatcher() {
+            public void onTextChanged(CharSequence str, int start, int before, int count) {
+                byte[] bytes = null;
+                try {
+                    bytes = str.toString().getBytes("KSC5601");
+                    int strCount = bytes.length;
+                    textView.setText(strCount + " / 80바이트");
+                } catch(UnsupportedEncodingException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            public void afterTextChanged(Editable strEditable) {
+                String str = strEditable.toString();
+                try {
+                    byte[] strBytes = str.getBytes("KSC5601");
+                    if(strBytes.length > 80) {
+                        strEditable.delete(strEditable.length()-2, strEditable.length()-1);
+                    }
+                } catch(Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        };
+        textView.addTextChangedListener(watcher);
+    }
+}
